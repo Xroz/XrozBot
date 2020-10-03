467558dd7a40129c9f4d25301c48e8b7d761dd3d
Ali-xD <54276125+Ali-xDYT@users.noreply.github.com>
2020-10-03 23:18:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const Client = require("./utils/Client");
-const client = new Client();
-client.start();
+require("dotenv").config()
+
+const { AkairoClient, CommandHandler } = require('discord-akairo');
+
+class MyClient extends AkairoClient {
+    constructor() {
+        super({
+            ownerID: ['ID', 'ID'],
+        }, {
+            disableMentions: 'everyone'
+        });
+
+        this.commandHandler = new CommandHandler(this, {
+            directory: './Commands/',
+            prefix: '!'
+        });
+        this.commandHandler.loadAll();
+
+    }
+}
+
+const client = new MyClient();
+client.login(process.env.TOKEN);
